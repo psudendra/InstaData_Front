342d6fd2913b7a49881f8d612124a0d2fa9122f0
Priya Sudendra <psudendra@gmail.com>
2015-04-25 14:53:53 -0600

    reverting to old file

--- instaPage.js
+++ instaPage.js
@@ -7,63 +7,52 @@ var myRegex = /((([h]+[t]+[t]+[p])+(\S)+([j]+[p]+[g]))|(([h]+[t]+[t]+[p])+(\S)+(
 var server = http.createServer(
 function( request, response ){
 
-res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Content-Length': html.length,
-    'Expires': new Date().toUTCString()
-  });
-res.end(html);
+  //lets require/import the mongodb native drivers.
+  var mongodb = require('mongodb');
 
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+  //We need to work with "MongoClient" interface in order to connect to a mongodb server.
+  var MongoClient = mongodb.MongoClient;
 
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+  // Connection URL. This is where your mongodb server is running.
+  var url = 'mongodb://localhost:27017/insta_data';
 
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:27017/insta_data';
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } 
-  else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-
-    // Get the documents collection
-    var collection = db.collection('current');
-
-    // Insert some users
-    //var result = [];
-    collection.findOne({},function (err, result) {
-      if (err) {
-        console.log(err);
-      }
-       else if (result) {
-         console.log('Found:', result);
-	 for (var i=0; i<result.current.length; i++) {
-           response.write(result['current'][i][1]+' \n');
-         }
-         response.end();
-       }
-    } 
-    else {
-      console.log('No document(s) found with defined "find" criteria!');
+  // Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
     }
 
-}
-}
-function buildHtml(req) {
-   var body = "<img src='" + current + "'>";
-  return '<DOCTYPE html>' + '<html><header></header>' + body +'</body></html>';
-      //Close connection
-   db.close();
+    else {
+      //HURRAY!! We are connected. :)
+      console.log('Connection established to', url);
+
+      // Get the documents collection
+      var collection = db.collection('current');
+
+      // Insert some users
+      //var result = [];
+      collection.findOne({},function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+        else if (result) {
+          for (var i=0; i<result.current.length; i++) {
+            response.write(result['current'][i][1]+' \n');
+          }
+
+          response.end();
+        }
+
+        else {
+        console.log('No document(s) found with defined "find" criteria!');
+        }
+      });
+    };
+    db.close();
+    
   });
 });
-});
-    // Regex
+// Regex
   
 // Point the HTTP server to port 50000.
 server.listen( 50000 );
