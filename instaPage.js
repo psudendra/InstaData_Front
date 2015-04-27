5da396381d8181e258ea95ef98735a9edabe9b98
Priya Sudendra <psudendra@gmail.com>
2015-04-27 15:11:45 -0600

    Formatting for page and added tag & like #s

--- instaPage.js
+++ instaPage.js
@@ -1,90 +1,99 @@
 // Load the necessary servers.
 var sys = require( "sys" );
 var http = require( "http" );
-var myRegex = /((([h]+[t]+[t]+[p])+(\S)+([j]+[p]+[g]))|(([h]+[t]+[t]+[p])+(\S)+([m]+[p]+[4])))/;
+//var myRegex = /((([h]+[t]+[t]+[p])+(\S)+([j]+[p]+[g]))|(([h]+[t]+[t]+[p])+(\S)+([m]+[p]+[4])))/;
 
 // Create our HTTP server.
 var server = http.createServer(
-function( request, response ){
-  response.writeHead(200, {"Content-Type" : "text/html"});
+	function( request, response ){
+		response.writeHead(200, {"Content-Type" : "text/html"});
 
-  var body = '\
-  <html>\
-  <head>\
-  <meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>\
-  <style>\
-    body {\
-      margin: auto;\
-      font: 14px Helvetica, Arial, sans-serif;\
-      color:#0000FF;\
-      text-align:center;\
-    }\
-\
-    a {\
-      color: #00B7FF;\
-    }\
-\
-    img {\
-      width: 200px;\
-      padding: 10px;\
-    }\
-\
-  </style>\
-  <title>InstaData</title>\
-  </head>\
-\
-  <body>\
-  <h1>Welcome to InstaData!</h1>';
-  response.write(body);
+		var body = '\
+		<html>\
+		<head>\
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>\
+		<style>\
+		body {\
+			margin: auto;\
+			font: 14px Helvetica, Arial, sans-serif;\
+			color:#0000FF;\
+			text-align:center;\
+		}\
+		\
+		a {\
+			color: #00B7FF;\
+		}\
+		\
+		img {\
+			width: 200px;\
+			padding: 10px;\
+		}\
+		\
+		.image {\
+			position:relative;\
+			display:inline-block;\
+		}\
+		</style>\
+		<title>InstaData</title>\
+		</head>\
+		\
+		<body>\
+		<h1>Welcome to InstaData!</h1>';
 
-  //lets require/import the mongodb native drivers.
-  var mongodb = require('mongodb');
+		response.write(body);
 
-  //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-  var MongoClient = mongodb.MongoClient;
+		//lets require/import the mongodb native drivers.
+		var mongodb = require('mongodb');
 
-  // Connection URL. This is where your mongodb server is running.
-  var url = 'mongodb://ec2-52-10-211-62.us-west-2.compute.amazonaws.com/insta_data';
+		//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+		var MongoClient = mongodb.MongoClient;
 
-  // Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    }
+		// Connection URL. This is where your mongodb server is running.
+		var url = 'mongodb://ec2-52-10-211-62.us-west-2.compute.amazonaws.com/insta_data';
 
-    else {
-      //HURRAY!! We are connected. :)
-      console.log('Connection established to', url);
+		// Use connect method to connect to the Server
+		MongoClient.connect(url, function (err, db) {
+			if (err) {
+		 		console.log('Unable to connect to the mongoDB server. Error:', err);
+		 	}
 
-      // Get the documents collection
-      var collection = db.collection('current');
+		 	else {
+		 		//HURRAY!! We are connected. :)
+		 		console.log('Connection established to', url);
 
-      // Insert some users
-      //var result = [];
-      collection.findOne({},function (err, result) {
-        if (err) {
-          console.log(err);
-        }
-        else if (result) {
-          for (var i=0; i<result.current.length; i++) {
-            response.write('<img src="' + result['current'][i][1] + '">' + ' \n');
-          }
+		 		// Get the documents collection
 
-          response.end();
-        }
+		 		var collection = db.collection('current');
 
-        else {
-        console.log('No document(s) found with defined "find" criteria!');
-        }
+		 		//var result = [];
 
+		 		collection.findOne({},function (err, result) {
+		 			if (err) {
+		 				console.log(err);
+		 			}
 
-      db.close();
-      });
-    };
-  });
-});
-// Regex
-  
+		 			else if (result) {
+
+				  		response.write(result["tag"] + '<br><br>');
+
+		 				for (var i=0; i<result.current.length; i++) {
+				  			response.write('<div class="image"><img src="' + result['current'][i][1] + '">' + '<br>' + "# of Likes: " + result['current'][i][0] + '</div>');
+				  		}
+
+				  		response.end();
+				  	}
+
+				  	else {
+				  		console.log('No document(s) found with defined "find" criteria!');
+				  	}
+
+				  	db.close();
+				});
+		 	};
+		});
+	}
+);
+
 // Point the HTTP server to port 50000.
 server.listen( 50000 );
  
